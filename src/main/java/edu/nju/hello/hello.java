36665f2a53d01b483084948aa9f30143666f2f5b
TraxexC <1156489606@qq.com>
2016-11-20 23:17:55 +0800

    1

---
+++ src/main/java/edu/nju/hello/hello.java
@@ -0,0 +1,9 @@
+package edu.nju.hello;
+
+public class hello {
+	
+	public void print() {
+		// TODO Auto-generated method stub
+		System.out.println("hello world!");
+	}
+}
---
+++ src/test/java/edu/nju/hello/helloTest.java
@@ -0,0 +1,13 @@
+package edu.nju.hello;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class helloTest {
+	
+	@Test
+	public void test() {
+		hello test=new hello();
+		test.print();
+	}
+}
